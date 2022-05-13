7fccde615e6569df11387bbf0be0b48c638b214d
Cristian Sancricca <cristiansancricca@gmail.com>
2022-05-13 10:19:50 -0300

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Cristian Sancricca on 13/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
